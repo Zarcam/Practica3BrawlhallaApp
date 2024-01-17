d0b5c18f746e392ace2a61f3a6a29f236b408216
zarcam <alejandrosatan.2896@gmail.com>
2024-01-17 20:31:20 +0100

    Added detail fragment

--- app/src/main/java/com/example/practica3brawlhallaapp/LegendDetailFragment.kt
+++ app/src/main/java/com/example/practica3brawlhallaapp/LegendDetailFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.net.toUri
 import androidx.fragment.app.activityViewModels
 
 class LegendDetailFragment : Fragment() {
@@ -22,7 +25,21 @@ class LegendDetailFragment : Fragment() {
         v = inflater.inflate(R.layout.fragment_legend_detail, container, false)
 
         this.legendViewModel.selected?.let {
-            //TODO: Cambiar TextViews del DetailFragment
+            v.findViewById<TextView>(R.id.legendDetailName).text = it.name
+
+            v.findViewById<TextView>(R.id.detailDamageStat).text = it.damage.toString()
+            v.findViewById<TextView>(R.id.detailDefenseStat).text = it.defense.toString()
+            v.findViewById<TextView>(R.id.detailDexterityStat).text = it.dexterity.toString()
+            v.findViewById<TextView>(R.id.detailSpeedStat).text = it.speed.toString()
+
+            v.findViewById<TextView>(R.id.detailweapon1).text = it.weapons[0]
+            v.findViewById<TextView>(R.id.detailweapon2).text = it.weapons[1]
+
+            v.findViewById<TextView>(R.id.detailLegendDesc).text = it.description
+
+            v.findViewById<ImageView>(R.id.legendImage)
+
+            //TODO: Cambiar imagen
         }
 
         return v
